f25e3e9885964f7c4c62df6cd900efa146a6e1d3
Jericho Guzman <mgmt06@gmail.com>
2016-09-03 14:57:52 -0400

    adding psuedocode for data processing

--- import_data.py
+++ import_data.py
@@ -2,4 +2,13 @@ import pandas as pd
 
 data = pd.read_csv("./train.csv")
 
-print data
+#lets fill in empty age fields with 29.7 the avergage age
+
+#lets drop the cabin column
+
+#drop the two rows where this is not a field for embarked
+
+#export modified data frame as cleaned.csw
+
+
+
