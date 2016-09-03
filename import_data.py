fdfd4ae9bb2c3e1dcddbb244d3dc92b70bc81180
Jericho Guzman <mgmt06@gmail.com>
2016-09-03 15:58:43 -0400

    pushing data munging

--- import_data.py
+++ import_data.py
@@ -1,14 +1,27 @@
 import pandas as pd 
+import math
 
 data = pd.read_csv("./train.csv")
 
 #lets fill in empty age fields with 29.7 the avergage age
 
+for index, value in enumerate(data["Age"]):
+	if math.isnan(value):
+		data["Age"][index] = 29.7
+
+# print data.info()
+
 #lets drop the cabin column
+data.drop("Cabin", axis=1, inplace=True)
+# print data.info()
 
 #drop the two rows where this is not a field for embarked
+data = data[pd.notnull(data['Embarked'])]
+# print data.info()
 
-#export modified data frame as cleaned.csw
+#export modified data frame as cleaned.csv
+data.to_csv("clean.csv")
+
 
 
 
